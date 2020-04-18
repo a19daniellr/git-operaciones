37ebdae93b5ec8ebbdbf822f51d550f71c810b75
a19daniellr <a19daniellr@iessanclemente.net>
2020-04-18 10:58:44 +0200

    Añadido una linea de texto con una operacion de multiplicacion

--- Principal.java
+++ Principal.java
@@ -7,5 +7,6 @@ public class Principal {
 		Operaciones tool = new Operaciones();
 		System.out.println(tool.multiplicar(2,5));
 		System.out.println(tool.division(5,2));
+		System.out.println(tool.multiplicar(10, 10));
 	}
 }
