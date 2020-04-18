938b658dd5b4886bcab3ec4972e2d250177e29f5
a19daniellr <a19daniellr@iessanclemente.net>
2020-04-18 10:50:16 +0200

    Mi primer commit

---
+++ Operaciones.java
@@ -0,0 +1,11 @@
+package operaciones;
+
+public class Operaciones {
+	public int multiplicar(int num, int num2) {
+		return num*num2;
+	}
+	public double division(int num, int num2) {
+		return (double)num/(double)num2;
+	}
+	
+}
---
+++ Principal.java
@@ -0,0 +1,11 @@
+package operaciones;
+
+import operaciones.Operaciones;
+
+public class Principal {
+	public static void main(String[] args) {
+		Operaciones tool = new Operaciones();
+		System.out.println(tool.multiplicar(2,5));
+		System.out.println(tool.division(5,2));
+	}
+}
